fdcfeacd5accd8a627a0e55178323fdfdf0f0fee
changes <joshkmotley@gmail.com>
2017-04-16 15:03:15 -0700

    add interactive pop transition

--- CustomTransitions/ViewController.m
+++ CustomTransitions/ViewController.m
@@ -13,6 +13,7 @@
 @interface ViewController () <UINavigationControllerDelegate>
 
 @property (strong, nonatomic) CustomAnimator *animator;
+@property (strong, nonatomic) UIPercentDrivenInteractiveTransition *interactionController;
 
 @end
 
@@ -23,14 +24,40 @@
     
     self.animator = [CustomAnimator new];
     self.navigationController.delegate = self;
+    
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(pan:)];
+    panGesture.cancelsTouchesInView = NO;
+    [self.navigationController.view addGestureRecognizer:panGesture];
+}
+
+-(void)pan:(UIPanGestureRecognizer *)recognizer{
+    if (recognizer.state == UIGestureRecognizerStateBegan) {
+        self.interactionController = [[UIPercentDrivenInteractiveTransition alloc]init];
+        CGPoint location = [recognizer locationInView:self.view];
+        if (location.x < CGRectGetMidX(self.view.frame)) {
+            [self.navigationController popViewControllerAnimated:YES];
+        }
+    }else if (recognizer.state == UIGestureRecognizerStateChanged) {
+        CGFloat d = ([recognizer translationInView:self.view].x / CGRectGetWidth(self.view.bounds)) * 1;
+        [self.interactionController updateInteractiveTransition:d];
+    }else if (recognizer.state == UIGestureRecognizerStateEnded) {
+        if ([recognizer velocityInView:self.view].x > 0) {
+            [self.interactionController finishInteractiveTransition];
+        } else {
+            [self.interactionController cancelInteractiveTransition];
+        }
+        self.interactionController = nil;
+    }
 }
 
 -(id<UIViewControllerAnimatedTransitioning>)navigationController:(UINavigationController *)navigationController animationControllerForOperation:(UINavigationControllerOperation)operation fromViewController:(UIViewController *)fromVC toViewController:(UIViewController *)toVC{
     
-    if (operation == UINavigationControllerOperationPush) {
-        return self.animator;
-    }
-    return nil;
+    return self.animator;
+}
+
+-(id<UIViewControllerInteractiveTransitioning>)navigationController:(UINavigationController *)navigationController interactionControllerForAnimationController:(id<UIViewControllerAnimatedTransitioning>)animationController{
+    
+    return self.interactionController;
 }
 
 -(void)viewDidAppear:(BOOL)animated{
